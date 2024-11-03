32fd3de45863001563c2f15863e4c899dcb038a8
dimasrdika <dimasrachmadika@gmail.com>
2024-11-03 12:46:13 +0700

    add: item products

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,22 +6,77 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const products = [
-  { title: "Alu foil Poe", img: "./products/alu-foil-poe.png" },
+  {
+    title: "Peel Off Lidding Alu Foil For POE",
+    img: "./products/alu-foil-poe.png",
+  },
   { title: "Amber Glass", img: "/products/amber-glass.jpg" },
+  {
+    title: "Pharmaceutical Bottle",
+    img: "/products/pharmaceutical-bottle.png",
+  },
   {
     title: "Pharmaceutical Bottle",
     img: "/products/pharmaceutical-bottle-1.png",
   },
   {
-    title: "Pharmaceutical Bottle",
-    img: "/products/pharmaceutical-bottle-2.png",
+    title: "AL/PE Strip Foil",
+    img: "/products/al-pe.png",
+  },
+  {
+    title: "Blister Alu Foil",
+    img: "/products/blister-alu-foil.png",
+  },
+  {
+    title: "Dropper Bottle",
+    img: "/products/dropper-bottle.png",
+  },
+  {
+    title: "Vials",
+    img: "/products/vials.png",
+  },
+  {
+    title: "Ampoules Bottle",
+    img: "/products/ampoules.png",
+  },
+  {
+    title: "Rubber Stopper",
+    img: "/products/rubber-stoppers.jpg",
+  },
+  {
+    title: "Pet Bottle",
+    img: "/products/pet-bottle.png",
+  },
+  {
+    title: "Cold Forming Foil",
+    img: "/products/cold-forming-foil.png",
+  },
+  {
+    title: "Child Resistant Foil",
+    img: "/products/child-resistant-foil.png",
+  },
+  {
+    title: "Lamination Film",
+    img: "/products/lamination-film.png",
+  },
+  {
+    title: "Rigid PVC, PVC/PVDC & PVC/PE",
+    img: "/products/rigid-pvc.png",
+  },
+  {
+    title: "Effervescent Tube",
+    img: "/products/effervescent-tube.jpg",
+  },
+  {
+    title: "Dropping Pils Tube",
+    img: "/products/dropping-pills.jpg",
   },
 ];
 
 const Product = () => {
   const settings = {
-    dots: true,
     infinite: true,
+    dots: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -44,15 +99,13 @@ const Product = () => {
                 className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
               >
                 <div className="flex justify-center w-full h-64">
-                  {" "}
-                  {/* Set a fixed height */}
                   <img
                     src={product.img}
                     alt={product.title}
-                    className="h-full w-auto object-contain rounded-lg shadow-md mb-4" // Object-contain to maintain aspect ratio
+                    className="h-full w-auto object-contain rounded-lg shadow-md mb-4"
                   />
                 </div>
-                <h2 className="mt-4 text-2xl font-semibold text-center">
+                <h2 className="mt-4 text-2xl font-semibold text-primary text-center">
                   {product.title}
                 </h2>
               </div>
